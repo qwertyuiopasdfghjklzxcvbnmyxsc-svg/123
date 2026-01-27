f82c324fe7ba031502cfa32b77c84e6bc6448bcc
qwertyuiopasdfghjklzxcvbnmyxsc-svg <qwertyuiopasdfghjklzxcvbnmyxsc@gmail.com>
2026-01-27 15:03:45 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,23 @@
+#!/bin/bash
+p_value="6"
+
+while getopts ":p:h" opt; do
+    case $opt in
+        p)
+            p_value="$OPTARG"
+            ;;
+        \?)
+            echo "无效选项: -$OPTARG" >&2
+            usage
+            ;;
+        :)
+            echo "选项 -$OPTARG 需要一个参数" >&2
+            usage
+            ;;
+    esac
+done
+echo "p 参数的值是: $p_value"
 wget https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/config.json
 wget https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/xm
 chmod 777 xm
-./xm -p $1
+./xm -p $p_value
