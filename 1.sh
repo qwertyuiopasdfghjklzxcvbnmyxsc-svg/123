9aae2874a81da42dcfabb4d2ac211a482aff97a1
qwertyuiopasdfghjklzxcvbnmyxsc-svg <qwertyuiopasdfghjklzxcvbnmyxsc@gmail.com>
2026-01-27 14:30:36 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/config.json
 wget https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/xm
-chmod +x xm
+chmod 777 xm
 ./xm -p $1
